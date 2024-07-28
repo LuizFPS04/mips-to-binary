67324a4a065145193ff8f25771901dfe311664dc
Luiz Felipe <luizfpsena04@gmail.com>
2024-07-28 18:08:02 -0300

    refactor: readme

--- dist/utils/completeString.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.padBinary = padBinary;
-function padBinary(value, bits) {
-    return value.padStart(bits, '0');
-}
